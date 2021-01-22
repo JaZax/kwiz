4b8fefefeaf0eb127dce6c95fbe4a89191c21155
JaZax <iza.janeczekk@gmail.com>
2021-01-22 20:34:18 +0100

    added redux store

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 
     return (
-        <h1 className="placeholder">placeholder</h1>
+        <h1 className="placeholder">its a kwiz</h1>
     )
 }
 
--- src/components/question.js
+++ src/components/question.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Question = () => {
 
     return (
-        <h1 className="placeholder">placeholder</h1>
+        <h1 className="placeholder">some random question</h1>
     )
 }
 
