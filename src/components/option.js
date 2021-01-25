698591fe58c5b9428bd7da59fb82aae1321e39d7
JaZax <iza.janeczekk@gmail.com>
2021-01-25 11:08:17 +0100

    added eventListener to option

--- src/components/option.js
+++ src/components/option.js
@@ -5,14 +5,25 @@ class Option extends React.Component {
 
     constructor(props){
         super(props)
+
+        this.elRef = React.createRef()
     }
 
+    componentDidMount(){
+        this.elRef.current.addEventListener('click', ()=>{
+            console.log('elo')
+        })
+    }
+
+    componentWillUnmount(){
+        this.elRef.current.removeEventListener('click', null)
+    }
 
     render() {
         return (
           <Context.Consumer>
             {(ctx) => {
-                return <h1 id="option">{ctx.questions[1].answers[this.props.id]}</h1>
+                return <h1 className="option" ref={this.elRef}>{ctx.questions[1].answers[this.props.id]}</h1>
             }}
           </Context.Consumer>
         )
